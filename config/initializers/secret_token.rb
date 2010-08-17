ddfa28d8bde965c0186823d4ddee4914a63a1e6d
Johnny Goodman <johnny.goodman@gmail.com>
2010-08-17 01:33:32 -0500

    maiden voyage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7a8f003b8039a316120d57c43f42fec943b1096a6d322452aa6b757483da395d907db936bdcc2d6c4067a794668e06bae79e95ff216a42799be8f423d4ee7357'
